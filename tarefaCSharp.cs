4f17100ac4963193528743226ce521fc9d370762
Erickk Santos <80436935+kcireST@users.noreply.github.com>
2024-06-05 23:50:05 -0300

    Add files via upload

---
+++ tarefaCSharp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número para ver a sua tabuada em ordem decrescente: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        for (int i = 10; i >= 1; i--)
+        {
+            Console.WriteLine($"{numero} x {i} = {numero * i}");
+        }
+    }
+}
