7caeb79e0261ff10cfd507cf906e2326d55ab0c0
Erickk Santos <80436935+kcireST@users.noreply.github.com>
2024-06-06 00:04:35 -0300

    Add files via upload

---
+++ FunçõesNumericas.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPgilmar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            double a, b, c, delta, x1, x2;
+
+            Console.Write("Entre com o valor a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Entre com o valor b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Entre com o valor c: ");
+            c = Convert.ToDouble(Console.ReadLine());
+            delta = Math.Pow(b, 2) - 4 * a * c;
+            x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            Console.WriteLine("X1: " + Math.Round(x1,2));
+            x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+            Console.WriteLine("X1: " + Math.Round(x2, 2));
+
+            Console.ReadKey();
+            */
+
+            /*
+            double r, area, peri, diame;
+            Console.Write("Entre com o valor do raio: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            area = Math.PI * Math.Pow(r, 2);
+            diame = r * 2;
+            peri = 2 * Math.PI * r;
+
+            Console.WriteLine("a area do circulo e:" + area);
+            Console.WriteLine("o diametro do circulo: " + diame);
+            Console.WriteLine("Perimetro: " + peri);
+            Console.ReadKey();*/
+        }
+    }
+}
